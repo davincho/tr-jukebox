28c295bc319740d618741f238727533f1bf6492d
David Madner <david@tourradar.com>
2020-04-16 09:23:19 +0200

    experiement with animations

--- src/pages/components/SoundButton.tsx
+++ src/pages/components/SoundButton.tsx
@@ -6,24 +6,39 @@ import { motion } from 'framer-motion';
 
 import styles from './button.module.scss';
 
+const combine = (input) =>
+  Object.keys(input)
+    .reduce((acc, key) => {
+      if (input[key]) {
+        acc.push(key);
+      }
+      return acc;
+    }, [])
+    .join(' ');
+
 const SoundButton: FC<{
   soundName: string;
 }> = ({ children, soundName }) => {
   const [isPlaying, setIsPlaying] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   return (
     <>
-      <motion.button
-        className={styles.btn}
+      <motion.div
+        className={combine({
+          [styles.btn]: true,
+        })}
         onClick={() => {
           setIsPlaying(true);
         }}
         whileTap={{ scale: 0.9 }}
       >
-        {children}
-      </motion.button>
+        <div className={styles.content}>{children}</div>
+      </motion.div>
       <Sound
         url={soundName}
+        onLoading={() => setIsLoading(true)}
+        onLoad={() => setIsLoading(false)}
         onStop={() => setIsPlaying(false)}
         onError={() => setIsPlaying(false)}
         onFinishedPlaying={() => setIsPlaying(false)}
