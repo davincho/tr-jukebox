1d37a2b9a4a8cae1c1a2abe256f9d29c976fd3b0
David Madner <david.madner@gmail.com>
2025-03-06 15:40:58 +0100

    chore: remove old typing files

--- remix.env.d.ts
+++
@@ -1,4 +0,0 @@
-/* eslint-disable spaced-comment */
-// Copyright © 2021 React Training LLC. All rights reserved.
-/// <reference types="@remix-run/dev" />
-/// <reference types="@remix-run/node/globals" />
